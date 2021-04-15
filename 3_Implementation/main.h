9d08f3e63f8ab91cbee632e0f8274a121739e090
dikshithcs <dikshithcs123@gmail.com>
2021-04-15 20:07:43 +0530

    Add files via upload

---
+++ 3_Implementation/main.h
@@ -0,0 +1,11 @@
+/*
+#include <stdio.h> ///for input output functions like printf, scanf
+#include <stdlib.h>
+#include <string.h>  ///string operations
+*/
+struct emp
+    {
+        char name[40]; ///name of employee
+        int age; /// age of employee
+        float bs; /// basic salary of employee
+    };
